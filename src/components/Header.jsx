6ba8f37182032b562a44bc37723cdaadc5cf95d3
Ritesh2412 <94961348+Ritesh2412@users.noreply.github.com>
2024-11-17 14:24:58 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+
+import {AppBar, Toolbar, styled} from '@mui/material';
+
+const Container= styled(AppBar)`
+    background: #060606;
+    height: 9vh;
+
+`
+
+const Header = ()=>{
+    return(
+        <Container position='static'>
+            <Toolbar>
+                CodePen
+            </Toolbar>
+        </Container>
+    )
+}
+
+export default Header;
